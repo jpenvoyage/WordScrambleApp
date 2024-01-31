b2c07507821af11fc0b312a251f1bf5ece6ede64
joaobug <joaoenfrance@gmail.com>
2024-01-31 11:58:19 -0300

    Project 5 Hacking with Swift

--- WordScrambleApp/ContentView.swift
+++ WordScrambleApp/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var newWord = ""
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                Section{
+                   TextField("Enter your word", text: $newWord)
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                    Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
         }
-        .padding()
+        .onAppear(perform: {
+            startGame()
+        })
+        
+        .onSubmit {
+            addNewWord()
+        }
+    }
+    
+    func addNewWord () {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {return}
+        
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+    
+    func startGame () {
+        if let startWord = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let rightWord = try? String(contentsOf: startWord) {
+                let allWords = rightWord.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "dsada"
+           return
+            }
+           
+        }
+        fatalError("aaaaaaa eerro")
     }
 }
 
