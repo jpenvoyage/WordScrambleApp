1c92df27037aab7b0a8cf8a856de752ec28dffa2
joaobug <joaoenfrance@gmail.com>
2024-01-31 11:12:40 -0300

    Initial Commit

---
+++ WordScrambleApp/WordScrambleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleAppApp.swift
+//  WordScrambleApp
+//
+//  Created by Joao Leal on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
