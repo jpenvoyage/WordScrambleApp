06844797c4acfd8b2d6a3664783cff2b7fd69f2a
joaobug <joaoenfrance@gmail.com>
2024-01-31 14:11:53 -0300

    add function isoriginal add function ireal

--- WordScrambleApp/ContentView.swift
+++ WordScrambleApp/ContentView.swift
@@ -13,6 +13,10 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     
+    @State private var errorTitle = ""
+    @State private var errorMsg = ""
+    @State private var showingError = false
+
     var body: some View {
         NavigationStack{
             List{
@@ -31,6 +35,11 @@ struct ContentView: View {
             startGame()
         })
         
+        .alert(errorTitle, isPresented: $showingError) {
+            Button("OK") { }
+        } message: {
+            Text(errorMsg)
+        }
         .onSubmit {
             addNewWord()
         }
@@ -40,6 +49,21 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard answer.count > 0 else {return}
+        
+        guard isOriginal(word: answer) else {
+            errorAlert (title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+       // guard isPossible(word: answer) else {
+         //   errorAlert (title: "Word not possible", message: "You cna't spell that //word from \(rootWord)")
+            //return
+        //}
+        
+        guard isReal(word: answer) else {
+            errorAlert (title: "no no no", message: "nonono")
+            return
+        }
         
         usedWords.insert(answer, at: 0)
         newWord = ""
@@ -56,6 +80,30 @@ struct ContentView: View {
         }
         fatalError("aaaaaaa eerro")
     }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        // how to check for mispelled words using UITextChecker
+        // step 1 create an instance of UITextChecker
+        let checker = UITextChecker()
+        // step 2 tell the checker how much of our string we want to check
+        let range = NSRange(location: 0, length: word.utf16.count)
+        // step 3 ask our checker where it found any mispellings
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "pt")
+        // step 4
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func errorAlert (title: String, message: String) {
+        errorTitle = title
+        errorMsg = message
+        showingError = true
+    }
+    
+    
 }
 
 #Preview {
